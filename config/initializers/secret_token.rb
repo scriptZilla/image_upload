4c20addf45f7f9c2b0ca877f16376283eb3654da
scriptZilla <david.lowery@colorado.edu>
2014-09-09 11:15:33 -0600

    Initializing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageUpload::Application.config.secret_key_base = '725a92cb358f5796488ebbebe2e0ca182efa0fdf9366be5f5607309945108803cc86b25a2d404bcc077437a1b76b231d2afe472a88c89ce9ca69fc1f3720364c'
